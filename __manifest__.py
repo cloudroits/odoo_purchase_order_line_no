47963fda5b61ccd081bc701ee864b22b5bc9b71e
Sherin Farhana <s.sherinfarhana@gmail.com>
2022-03-24 18:13:48 +0300

    Version 13.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Serial no for product lines in Purchase Order lines',
-    'version': '14.0.0.0',
+    'version': '13.0.0.0',
     'author': 'Cloudroits',
     'summary': 'Add serial/line nos to product lines in Purchase Orders',
     'description': """This module displays serial/line nos to product lines in Purchase Orders both in the form as well in reports.""",
